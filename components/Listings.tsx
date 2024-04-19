51fc38616b21692f93ad14971a00de278785733d
MavisOdis <odivwofene@gmail.com>
2024-04-19 19:19:07 +0100

    Listing page key modified

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,24 +1,44 @@
-import { View, FlatList, ListRenderItem, TouchableOpacity, Image } from 'react-native'
-import React from 'react';
-import { ListingType } from '@/types/listingType';
+import {
+  View,
+  FlatList,
+  ListRenderItem,
+  TouchableOpacity,
+  Image,
+  Text,
+} from "react-native";
+import React from "react";
+import { ListingType } from "@/types/listingType";
 
 type Props = {
   listings: any[]
-}
+};
 
-export default function Listings({listings}: Props) {
+export default function Listings({ listings }: Props) {
   const renderItems: ListRenderItem<ListingType> = ({ item }) => {
-    return(
+    return (
       <TouchableOpacity>
         <View>
-          <Image source={{uri: item.image}} style={{width:200, height:200}}/>
+          <Image
+            source={{ uri: item.image }}
+            style={{ width: 200, height: 200 }}
+          />
+          <Text>{item.name}</Text>
         </View>
       </TouchableOpacity>
-    )
-  }
+    );
+  };
+
+  const keyExtractor = (item: ListingType, index: number) => index.toString();
+
   return (
     <View>
-      <FlatList data={listings} renderItem={renderItems} horizontal showsHorizontalScrollIndicator={false} />
+      <FlatList
+        data={listings}
+        renderItem={renderItems}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={keyExtractor}
+      />
     </View>
-  )
+  );
 }
