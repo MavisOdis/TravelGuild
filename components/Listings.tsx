4e395804069a6bb8a6e0f6bf48c0ff06d03c8ce3
MavisOdis <odivwofene@gmail.com>
2024-04-19 20:10:12 +0100

    Listing page key modified

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -5,9 +5,12 @@ import {
   TouchableOpacity,
   Image,
   Text,
+  StyleSheet,
 } from "react-native";
 import React from "react";
 import { ListingType } from "@/types/listingType";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 type Props = {
   listings: any[]
@@ -16,15 +19,20 @@ type Props = {
 export default function Listings({ listings }: Props) {
   const renderItems: ListRenderItem<ListingType> = ({ item }) => {
     return (
+      <>
       <TouchableOpacity>
-        <View>
+        <View style={styles.iteam}>
           <Image
             source={{ uri: item.image }}
-            style={{ width: 200, height: 200 }}
+            style={styles.image}
           />
-          <Text>{item.name}</Text>
+          <View>
+            <Ionicons name="bookmark" size={20} style={styles.bookmark}/>
+          </View>
+          
         </View>
       </TouchableOpacity>
+    </>
     );
   };
 
@@ -42,3 +50,30 @@ export default function Listings({ listings }: Props) {
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  iteam: {
+    backgroundColor: Colors.white,
+    padding: 20,
+    borderRadius: 10,
+    margin: 10,
+    width: 220
+  },
+  image: {
+    width: "100%", 
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 30
+  },
+  bookmark: {
+    position: 'absolute',
+    top: 185,
+    right: 30,
+    backgroundColor: Colors.primaryColor,
+    padding: 10,
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: Colors.white
+  }
+})
