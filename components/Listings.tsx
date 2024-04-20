b5325e2fe05074432eeb277fe0fc9341dea1b7f7
MavisOdis <odivwofene@gmail.com>
2024-04-20 21:31:10 +0100

    Listing page completed

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -10,29 +10,43 @@ import {
 import React from "react";
 import { ListingType } from "@/types/listingType";
 import Colors from "@/constants/Colors";
-import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 type Props = {
-  listings: any[]
+  listings: any[];
 };
 
 export default function Listings({ listings }: Props) {
   const renderItems: ListRenderItem<ListingType> = ({ item }) => {
     return (
-      <>
-      <TouchableOpacity>
-        <View style={styles.iteam}>
-          <Image
-            source={{ uri: item.image }}
-            style={styles.image}
-          />
-          <View>
-            <Ionicons name="bookmark" size={20} style={styles.bookmark}/>
+      <Link href={`/listing/${item.id}`} asChild>
+        <TouchableOpacity>
+          <View style={styles.iteam}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+
+            <View style={styles.bookmark}>
+              <Ionicons
+                name="bookmark-outline"
+                size={20}
+                color={Colors.white}
+              />
+            </View>
+
+            <Text style={styles.itemTxt} numberOfLines={1} ellipsizeMode="tail">
+              {item.name}
+            </Text>
+
+            <View style={{flexDirection: "row", justifyContent:"space-between"}}>
+              <View style={{flexDirection:'row',alignItems:'center'}}>
+                <FontAwesome5 name="map-marker-alt" size={18} color={Colors.primaryColor}/>
+                <Text style={styles.itemLocation}>{item.location}</Text>
+              </View>
+              <Text style={styles.itemPriceTxt}>${item.price}</Text>
+            </View>
           </View>
-          
-        </View>
-      </TouchableOpacity>
-    </>
+        </TouchableOpacity>
+      </Link>
     );
   };
 
@@ -51,29 +65,43 @@ export default function Listings({ listings }: Props) {
   );
 }
 
-
 const styles = StyleSheet.create({
   iteam: {
     backgroundColor: Colors.white,
     padding: 20,
     borderRadius: 10,
     margin: 10,
-    width: 220
+    width: 220,
   },
   image: {
-    width: "100%", 
+    width: "100%",
     height: 200,
     borderRadius: 10,
-    marginBottom: 30
+    marginBottom: 15,
   },
   bookmark: {
-    position: 'absolute',
+    position: "absolute",
     top: 185,
     right: 30,
     backgroundColor: Colors.primaryColor,
     padding: 10,
     borderRadius: 30,
     borderWidth: 2,
-    borderColor: Colors.white
-  }
-})
+    borderColor: Colors.white,
+  },
+  itemTxt: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: Colors.black,
+    marginBottom: 10,
+  },
+  itemLocation: {
+    fontSize: 12,
+    marginLeft: 5
+  },
+  itemPriceTxt: {
+    fontSize: 12,
+    fontWeight: '600',
+    color: Colors.primaryColor
+  },
+});
