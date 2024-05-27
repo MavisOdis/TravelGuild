43e6f3010af7b151323b799b7d948f896a9c95b7
MavisOdis <odivwofene@gmail.com>
2024-05-27 21:01:51 +0100

    Details page started

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,14 +1,60 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { ListingType } from "@/types/listingType";
+import ListingData from "../../data/destinations.json";
+import { Feather } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+
+const { width } = Dimensions.get("window");
+const IMG_HEIGHT = 300;
 
 const ListingDetails = () => {
   const { id } = useLocalSearchParams();
+  const listing: ListingType = (ListingData as ListingType[]).find(
+    (item) => item.id === id
+  );
+
   return (
-    <View>
-      <Text>ListingDetails{id}</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => {}}>
+              <View
+                style={{
+                  backgroundColor: Colors.white,
+                  padding: 6,
+                  borderRadius: 10,
+                }}
+              >
+                <Feather name="arrow-left" size={20} />
+              </View>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <View>
+        <Image source={{ uri: listing.image }} style={styles.image} />
+      </View>
+    </>
   );
 };
 
 export default ListingDetails;
+
+const styles = StyleSheet.create({
+  image: {
+    width: width,
+    height: IMG_HEIGHT,
+  },
+});
